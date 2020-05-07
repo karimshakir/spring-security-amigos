28bf46d6e72edbffee092aee0edbf4e9105f013b
“karimshakir” <karimshakir99@gmail.com>
2020-05-06 20:50:56 -0500

    initial push

---
+++ src/main/java/com/example/springsecurityamigos/SpringSecurityAmigosApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAmigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAmigosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityamigos/SpringSecurityAmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityamigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAmigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
