01d039d018df80d08264db53957bcdf7083149fc
“karimshakir” <karimshakir99@gmail.com>
2020-05-09 10:37:06 -0500

    start session id

--- src/main/java/com/example/springsecurityamigos/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurityamigos/security/ApplicationSecurityConfig.java
@@ -29,18 +29,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
           http
+//                  .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                  .and()
                   .csrf().disable()
                   .authorizeRequests()
                   .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                   .antMatchers("/api/**").hasRole(STUDENT.name())
-//                  .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                  .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                  .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                  .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                   .anyRequest()
                   .authenticated()
                   .and()
-                  .httpBasic();
+                  .formLogin();
     }
 
     //Retrieve users from database.
