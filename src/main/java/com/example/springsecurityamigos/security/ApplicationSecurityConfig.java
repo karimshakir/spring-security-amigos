506534e5ccb4f04235adba70b49a9d32d26e947c
“karimshakir” <karimshakir99@gmail.com>
2020-05-09 19:40:29 -0500

    complete form-base-authentication

--- src/main/java/com/example/springsecurityamigos/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurityamigos/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
@@ -41,12 +42,24 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                   .authenticated()
                   .and()
                   .formLogin()
-                  .loginPage("/login").permitAll()
-                  .defaultSuccessUrl("/courses", true)
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/courses", true)
+                    .passwordParameter("password")
+                    .usernameParameter("username")
                   .and()
                   .rememberMe()
                     .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-                    .key("somthingverysecured");
+                    .key("somthingverysecured")
+                    .rememberMeParameter("remember-me")
+                  .and()
+                  .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
     }
 
     //Retrieve users from database.
